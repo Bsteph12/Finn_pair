db5cd9f09d39bbf61759d334edd0dfd6eac2103b
Bsteph12 <109040589+Bsteph12@users.noreply.github.com>
2025-07-22 12:04:07 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const { connectToWhatsApp } = require('./bot');
 
-global.qrCodes = {};
+global.pairCodes = {};
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,18 +15,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('index', { qr: null, number: null });
+  res.render('index', { code: null, number: null });
 });
 
 app.post('/pair', async (req, res) => {
-    const number = req.body.number;
-    if (!number) return res.send('❌ Numéro requis !');
+  const number = req.body.number;
+  if (!number) return res.send('❌ Numéro requis !');
 
-    await connectToWhatsApp(number);
-    setTimeout(() => {
-        const qr = global.qrCodes[number] || null;
-        res.render('index', { qr, number });
-    }, 2000);
+  await connectToWhatsApp(number);
+
+  setTimeout(() => {
+    const code = global.pairCodes[number] || null;
+    res.render('index', { code, number });
+  }, 2000);
 });
 
 app.listen(port, () => console.log(`🌐 Serveur lancé sur http://localhost:${port}`));
